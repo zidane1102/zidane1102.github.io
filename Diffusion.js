0dac271150fbf9f5964edabb7d4ff385ef6f3d36
zidane1102 <68657910+zidane1102@users.noreply.github.com>
2023-06-03 21:45:14 +0700

    Add files via upload

--- Diffusion.js
+++ Diffusion.js
@@ -2,46 +2,42 @@ account = `sk-bO1o08nghUYsgn8OqwCJhqm2YOu0C4nUsBujnRFSjoqQBSvY`
 $(document).on('click', '#Random', function(){
     document.getElementById('seed').value = Math.floor(Math.random() * 1000000)
 })
+var height = null
+var width = null
+$(document).on('click', '#dimensions', function (){
+    $(this).toggleClass('focus')
+    height = 512
+    width = 768
+})
 $(document).on('click', '#Generate', function (){
     var prompt = {}
     var postprompt = {}
     var negprompt = {}
     let engine_id = null
-    params = ["height", "width", "cfg_scale", "sampler", "samples", "seed", "steps", "style_preset"]
+    params = ["cfg_scale", "sampler", "samples", "seed", "steps", "style_preset"]
     engine_id = document.getElementById("model-select").value
     let input = document.getElementById("prompting").value
     let neginput = document.getElementById("negative-prompting").value
     let postweight = document.getElementById("post-weight").value
     let negweight = document.getElementById("neg-weight").value
-    let height = document.getElementById("height").value
-    let width = document.getElementById("width").value
     let cfg = document.getElementById("cfg_scale").value
     postprompt["text"] = input
     postprompt["weight"] = parseFloat(postweight)
     negprompt["text"] = neginput
     negprompt["weight"] = parseFloat(negweight)
     prompt["text_prompts"] = [postprompt, negprompt]
-    debugger
+    prompt["height"] = height
+    prompt["width"] = width
     for (const pa of params) {
         console.log(pa)
-        if (pa == "height" || pa == "width" || pa == "cfg_scale" || pa == "samples" || pa == "seed" || pa == "steps"){
+        if (pa == "cfg_scale" || pa == "samples" || pa == "seed" || pa == "steps"){
             prompt[pa] = parseInt(document.getElementById(`${pa}`).value)
         }
         else{
         prompt[pa] = document.getElementById(`${pa}`).value;
         }
     }
-    if (height > 1024 || height < 128 || !height || height == 'null'){
-        $("#height").addClass('red')
-        $("#height").attr('title', 'Giá trị bạn vừa nhập phải lớn hơn 128 và bé hơn 1024')
-        return 
-    }
-    else if (width > 1024 || width < 128 || !width || width == 'null'){
-        $("#width").addClass('red')
-        $("#width").attr('title', 'Giá trị bạn vừa nhập phải lớn hơn 128 và bé hơn 1024')
-        return 
-    }
-    else if (cfg > 35 || cfg < 0 || !cfg || cfg == 'null'){
+    if (cfg > 35 || cfg < 0 || !cfg || cfg == 'null'){
         $("#cfg_scale").addClass('red')
         $("#cfg_scale").attr('title', 'Giá trị bạn vừa nhập phải lớn hơn 0 và bé hơn 35')
         return 
